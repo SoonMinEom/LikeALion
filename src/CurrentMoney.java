7e8bf966bff8fef9786b994d3d2e51e2667d6f77
soonmineom <skdlfma123@gmail.com>
2022-10-04 14:23:08 +0900

    화패 매수2

--- src/CurrentMoney.java
+++ src/CurrentMoney.java
@@ -7,9 +7,13 @@ public class CurrentMoney {
 
         int refund = 25000;
 
-        System.out.printf("5만원권 %d장\n", refund / curr50000);
-        System.out.printf("1만원권 %d장\n", refund / curr10000);
-        System.out.printf("5천원권 %d장\n", refund / curr5000);
-        System.out.printf("1천원권 %d장\n", refund / curr1000);
+        System.out.printf("5만원권 %d장", refund / curr50000 );
+        System.out.printf("  나머지 금액 : %d원\n", refund % curr50000);
+        System.out.printf("1만원권 %d장", refund / curr10000);
+        System.out.printf("  나머지 금액 : %d원\n", refund % curr10000);
+        System.out.printf("5천원권 %d장", refund / curr5000);
+        System.out.printf("  나머지 금액 : %d원\n", refund % curr5000);
+        System.out.printf("1천원권 %d장", refund / curr1000);
+        System.out.printf("  나머지 금액 : %d원\n", refund % curr1000);
     }
 }
