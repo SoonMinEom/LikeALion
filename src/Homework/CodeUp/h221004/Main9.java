f366d28a541aed769b5639a11398423a11b00f89
soonmineom <skdlfma123@gmail.com>
2022-10-04 17:22:54 +0900

    기본 출력 문제9 정수 입력과 출력

---
+++ src/Homework/CodeUp/h221004/Main9.java
@@ -0,0 +1,12 @@
+package Homework.CodeUp.h221004;
+
+import java.util.Scanner;
+
+public class Main9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
