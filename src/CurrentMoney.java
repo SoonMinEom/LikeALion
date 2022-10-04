8ad85176b36a6cc093487b5002a61e19b5df7dde
soonmineom <skdlfma123@gmail.com>
2022-10-04 14:26:29 +0900

    몫과 나머지 구하기 1. 50000~500까지 몫과 나머지 2. 생각떠올리기

--- src/CurrentMoney.java
+++ src/CurrentMoney.java
@@ -4,8 +4,9 @@ public class CurrentMoney {
         int curr10000 = 10000;
         int curr5000 = 5000;
         int curr1000 = 1000;
+        int curr500 = 500;
 
-        int refund = 25000;
+        int refund = 27600;
 
         System.out.printf("5만원권 %d장", refund / curr50000 );
         System.out.printf("  나머지 금액 : %d원\n", refund % curr50000);
@@ -15,5 +16,7 @@ public class CurrentMoney {
         System.out.printf("  나머지 금액 : %d원\n", refund % curr5000);
         System.out.printf("1천원권 %d장", refund / curr1000);
         System.out.printf("  나머지 금액 : %d원\n", refund % curr1000);
+        System.out.printf("500원권 %d장", refund / curr500);
+        System.out.printf("  나머지 금액 : %d원\n", refund % curr500);
     }
 }
