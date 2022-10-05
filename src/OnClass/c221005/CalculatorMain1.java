8ef4776f06f1d75dce849985f1a7d3daebee0819
SoonMinEom <114646711+SoonMinEom@users.noreply.github.com>
2022-10-05 10:48:53 +0900

    Update CalculatorMain1.java

--- src/OnClass/c221005/CalculatorMain1.java
+++ src/OnClass/c221005/CalculatorMain1.java
@@ -4,7 +4,9 @@ public class CalculatorMain1 {
     public static void main(String[] args) {
 
         Calculator1 cal = new Calculator1(20,10);
-
+        
+        // 생성자에 한 번만 매개변수를 입력하면 됨.
+        
         cal.Plus();
         cal.Minus();
         cal.Multiple();
