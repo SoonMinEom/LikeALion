c7fd4a0f84754d9e3cc135c6b7ee85cbcd7a6011
soonmineom <skdlfma123@gmail.com>
2022-10-05 20:54:50 +0900

    공백으로 구분된 두 char 타입 자리바꿔 출력 당황하지말고, next().charAt() 두 번사용

---
+++ src/Homework/CodeUp/h221005/Main4.java
@@ -0,0 +1,14 @@
+package Homework.CodeUp.h221005;
+
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char c1 = sc.next().charAt(0);
+        char c2 = sc.next().charAt(0);
+
+        System.out.printf("%c %c", c2, c1);
+    }
+}
