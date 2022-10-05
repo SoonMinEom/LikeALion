10d7b2ecd7a03530f691279250f883634aaa5e51
soonmineom <skdlfma123@gmail.com>
2022-10-05 10:18:13 +0900

    계산기 클래스

---
+++ src/OnClass/c221005/Calculator.java
@@ -0,0 +1,19 @@
+package OnClass.c221005;
+
+public class Calculator {
+    public void Plus (int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public void Minus (int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public void Multiple (int a, int b) {
+        System.out.println(a * b);
+    }
+
+    public void Divide (int a, int b) {
+        System.out.println(a / b);
+    }
+}
