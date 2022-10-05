d1e8c4d476d32d4cf8f0303dc3bd9f33f1a7d15f
soonmineom <skdlfma123@gmail.com>
2022-10-05 20:45:55 +0900

    float 타입 입출력 소숫점 뒤 0을 출력하려면 printf("%f", f); 사용하기

---
+++ src/Homework/CodeUp/h221005/Main2.java
@@ -0,0 +1,12 @@
+package Homework.CodeUp.h221005;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float f = sc.nextFloat();
+
+        System.out.printf("%f", f);
+    }
+}
