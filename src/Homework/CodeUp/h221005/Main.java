1db4eed3ae513763f432d69cb167506748c8ecd3
soonmineom <skdlfma123@gmail.com>
2022-10-05 20:30:29 +0900

    Char 타입 입출력 Scanner의 .next().charAt(0) 사용하기

---
+++ src/Homework/CodeUp/h221005/Main.java
@@ -0,0 +1,12 @@
+package Homework.CodeUp.h221005;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = (char) sc.next().charAt(0);
+
+        System.out.println(ch);
+    }
+}
