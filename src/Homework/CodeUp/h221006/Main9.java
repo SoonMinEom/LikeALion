225ffa39e9edbf4f5f7a15412014b7f4d8f0971d
soonmineom <skdlfma123@gmail.com>
2022-10-06 13:23:15 +0900

    String.format("포멧", 입력값) 활용

---
+++ src/Homework/CodeUp/h221006/Main9.java
@@ -0,0 +1,12 @@
+package Homework.CodeUp.h221006;
+
+import java.util.Scanner;
+
+public class Main9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble();
+
+        System.out.println(String.format("%.11f", num));
+    }
+}
