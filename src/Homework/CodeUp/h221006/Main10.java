b5fac66ab4fd528ad836f15ab933872802c00213
soonmineom <skdlfma123@gmail.com>
2022-10-06 13:50:51 +0900

    BigInteger 클래스 활용.

---
+++ src/Homework/CodeUp/h221006/Main10.java
@@ -0,0 +1,13 @@
+package Homework.CodeUp.h221006;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger num = sc.nextBigInteger();
+
+        System.out.println(num);
+    }
+}
