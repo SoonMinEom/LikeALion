692e54745a7c38f122ed5ac5c6889c4ffdd6834b
soonmineom <skdlfma123@gmail.com>
2022-10-06 12:51:14 +0900

    long 타입 활용

---
+++ src/Homework/CodeUp/h221006/Main8.java
@@ -0,0 +1,12 @@
+package Homework.CodeUp.h221006;
+
+import java.util.Scanner;
+
+public class Main8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+
+        System.out.println(num);
+    }
+}
