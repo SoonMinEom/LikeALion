c2c6af0df61c843ff6d199e9c03ea780c5faacc7
soonmineom <skdlfma123@gmail.com>
2022-10-07 10:35:49 +0900

    Parse 메소드 구현

---
+++ src/OnClass/c221007/PopulationMove.java
@@ -0,0 +1,25 @@
+package OnClass.c221007;
+
+public class PopulationMove {
+    private int fromSido;
+    private int toSido;
+
+    // 생성자
+    public PopulationMove(int fromSido, int toSido) {
+        this.fromSido = fromSido;
+        this.toSido = toSido;
+    }
+
+    public int getFromSido() {
+        return fromSido;
+    }
+
+    public int getToSido() {
+        return toSido;
+    }
+
+    public PopulationMove PopulationMoveParse (String data) {
+        String[] sidoData = data.split("//,");
+        return new PopulationMove(Integer.parseInt(sidoData[0]), Integer.parseInt(sidoData[6]));
+    }
+}
