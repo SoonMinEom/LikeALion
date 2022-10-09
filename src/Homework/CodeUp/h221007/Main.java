3f32bddc9916ad70c118aa88a0aee7674071d0f5
soonmineom <skdlfma123@gmail.com>
2022-10-09 11:32:55 +0900

    10진수 입력, 8진수 출력 souf("%o", 10진수);

---
+++ src/Homework/CodeUp/h221007/Main.java
@@ -0,0 +1,11 @@
+package Homework.CodeUp.h221007;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.printf("%o", input);
+    }
+}
