a0d2be58e1516034ac4b492945a32d5ff887ae91
soonmineom <skdlfma123@gmail.com>
2022-10-09 12:04:22 +0900

    부호바꾸기

---
+++ src/Homework/CodeUp/h221007/Main10.java
@@ -0,0 +1,13 @@
+package Homework.CodeUp.h221007;
+
+import java.util.Scanner;
+
+public class Main10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        System.out.println(-a);
+    }
+}
