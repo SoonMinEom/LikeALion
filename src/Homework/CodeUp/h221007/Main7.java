6bcc83f8ea6804dffd78f78954d8c9607827dd64
soonmineom <skdlfma123@gmail.com>
2022-10-09 11:56:21 +0900

    숫자 입력 받고, 아스키 문자 출력

---
+++ src/Homework/CodeUp/h221007/Main7.java
@@ -0,0 +1,12 @@
+package Homework.CodeUp.h221007;
+
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        char c = (char) input;
+        System.out.println(c);
+    }
+}
