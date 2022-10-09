23fb4625dcb5e5ca90f74705e13d4073a3d13b60
soonmineom <skdlfma123@gmail.com>
2022-10-09 11:54:27 +0900

    문자 입력 받고, 아스키 코드 출력

---
+++ src/Homework/CodeUp/h221007/Main6.java
@@ -0,0 +1,12 @@
+package Homework.CodeUp.h221007;
+
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int c = s.charAt(0);
+        System.out.println(c);
+    }
+}
