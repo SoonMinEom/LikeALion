b304d3456f77095db145269be696e6263d0946e5
soonmineom <skdlfma123@gmail.com>
2022-10-09 12:01:28 +0900

    계산 결과가 int 범위를 넘어갈 때. long타입 사용

---
+++ src/Homework/CodeUp/h221007/Main8.java
@@ -0,0 +1,14 @@
+package Homework.CodeUp.h221007;
+
+import java.util.Scanner;
+
+public class Main8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        System.out.println(a+b);
+    }
+}
