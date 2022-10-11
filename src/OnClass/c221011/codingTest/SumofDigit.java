246cc35678fb17a2184c0833c3b39de4fa8330dd
soonmineom <skdlfma123@gmail.com>
2022-10-11 09:45:43 +0900

    자릿수 더하기(+강사님)

--- src/OnClass/c221011/codingTest/SumofDigit.java
+++ src/OnClass/c221011/codingTest/SumofDigit.java
@@ -8,16 +8,30 @@ public class SumofDigit {
         int sum = 0;
 
         for (int i = length.length()-1; i >=0 ; i--) {
-            int temp = input / (int )Math.pow(10, i);
-            input -= temp*(int )Math.pow(10, i);
+            int temp = input / (int)Math.pow(10, i);
+            input -= temp*(int)Math.pow(10, i);
             sum += temp;
         }
         return sum;
     }
+
+    public int solution2 (int n) {
+        int result = 0 ;
+
+        while (n>0) {
+            result += n % 10;
+            n = n / 10;
+//          System.out.printf("origin:%d result:%d\n", n, result);
+        }
+        return result;
+    }
     public static void main(String[] args) {
 
         SumofDigit sod = new SumofDigit();
-        int result = sod.solution(1234);
+        int result = sod.solution(12345);
+        int result2 = sod.solution2(12345);
+
+        System.out.println(result2);
 
         if (result == 10) {
             System.out.println("테스트 통과");
