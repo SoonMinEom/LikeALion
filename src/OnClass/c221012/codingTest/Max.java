22e6ccff325bec57ccf915339538723d15190d3d
soonmineom <skdlfma123@gmail.com>
2022-10-12 10:44:15 +0900

    최대값 구하기, 2차원 배열 최대값 구하기

--- src/OnClass/c221012/codingTest/Max.java
+++ src/OnClass/c221012/codingTest/Max.java
@@ -11,8 +11,11 @@ public class Max {
     public int getMax(int[] arr) {
 
         int max = 0;
+        // int max = arr[0]; -> 배열의 값이 음수여도 문제 없음!!
 
         for (int i = 0 ; i <arr.length ; i ++) {
+      //for (int i = 1 ; i <arr.length ; i ++)
+      //arr[0]이 초기화 값이니, arr[1]부터 루프하면 됨.
             if(arr[i]>max) {
                 max = arr[i];
             }
@@ -31,6 +34,12 @@ public class Max {
         } else {
             System.out.println("테스트 실패");
         }
+
+// 이 로직에서 발생할 수 있는 문제점과 개선점.
+// 1. 배열의 값이 모두 음수일 경우 : max 초기화 값이 0 이라 결과도 0이 나옴.
+// -> 초기화 값을 arr[0]으로 하면 된다.
+// -> 초기화 값이 arr[0]이니까, 루프는 arr[1]부터 하면 됨. (비교값이 많아질 수록 효율 급상승)
+
 
     }
 }
