1fd66a307373650ae3e8985be13024097bf133f9
soonmineom <skdlfma123@gmail.com>
2022-10-13 12:54:17 +0900

    switch문

---
+++ src/Homework/CodeUp/h221013/Main9.java
@@ -0,0 +1,30 @@
+package Homework.CodeUp.h221013;
+
+import java.util.Scanner;
+
+public class Main9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+
+
+        switch (a) {
+            case 'A':
+                System.out.println("best!!!");
+                break;
+            case 'B':
+                System.out.println("good!!");
+                break;
+            case 'C':
+                System.out.println("run!");
+                break;
+            case 'D':
+                System.out.println("slowly~");
+                break;
+            default:
+                System.out.println("what?");
+                break;
+        }
+
+    }
+}
