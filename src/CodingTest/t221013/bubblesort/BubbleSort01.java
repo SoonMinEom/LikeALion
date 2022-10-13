82d2bf3a293276939e420ac939a4b0fba4cde740
soonmineom <skdlfma123@gmail.com>
2022-10-13 10:08:40 +0900

    버블정렬 실습

--- src/CodingTest/t221013/bubblesort/BubbleSort01.java
+++ src/CodingTest/t221013/bubblesort/BubbleSort01.java
@@ -12,12 +12,13 @@ public class BubbleSort01 {
 
     public int[] BubbleSort(int[] arr) {
 
-        for (int i = 0 ; i < arr.length ; i ++) {
-            for (int j = i ; j < arr.length-1 ; j++) {
-                if( arr[j] > arr[j+1] ) {
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = temp;
+        // 강사님 방식. 떨어진 것끼리 교환.
+        for (int i = 0 ; i < arr.length-1 ; i ++) {
+            for (int j = i ; j < arr.length ; j++) {
+                if( arr[i] > arr[j] ) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
                 }
             }
         }
