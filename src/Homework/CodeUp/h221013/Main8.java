88bf130ecb65311c61ae6b118027446db0cd2522
soonmineom <skdlfma123@gmail.com>
2022-10-13 12:50:13 +0900

    else if 활용

---
+++ src/Homework/CodeUp/h221013/Main8.java
@@ -0,0 +1,22 @@
+package Homework.CodeUp.h221013;
+
+import java.util.Scanner;
+
+public class Main8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+
+        if ( a >=90 ) {
+            System.out.println("A");
+        } else if ( a >= 70) {
+            System.out.println("B");
+        } else if ( a >= 40) {
+            System.out.println("C");
+        } else  {
+            System.out.println("D");
+        }
+
+    }
+}
