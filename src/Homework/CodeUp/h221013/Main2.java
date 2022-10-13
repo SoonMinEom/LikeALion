e89b125dd1a90f2a5fc76528d8e2c956169ab023
soonmineom <skdlfma123@gmail.com>
2022-10-13 12:34:03 +0900

    비트연산자 ^ : 둘이 서로 달라야 1

---
+++ src/Homework/CodeUp/h221013/Main2.java
@@ -0,0 +1,14 @@
+package Homework.CodeUp.h221013;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        long result = a ^ b;
+
+        System.out.println(result);
+    }
+}
