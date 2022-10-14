e7458c8c33dfbf4160c584bcb96903a3c203e47f
soonmineom <skdlfma123@gmail.com>
2022-10-14 10:19:20 +0900

    InsertionSort 완성

--- src/CodingTest/t221014/insertionSort/InsertionSort01.java
+++ src/CodingTest/t221014/insertionSort/InsertionSort01.java
@@ -3,28 +3,39 @@ package CodingTest.t221014.insertionSort;
 import java.util.Arrays;
 
 public class InsertionSort01 {
-
-    public int[] sort (int[] arr) {
-        int i = 1;
-        if (arr[i] < arr[i-1]) {
-            int temp = arr[i-1];
-            arr[i-1] = arr[i];
-            arr[i] = temp;
+    public int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+//            for ( int j = i+1 ; j >0 ; j-- ) {
+//                if (arr[j] < arr [j-1]) {
+//                    int temp = arr[j - 1];
+//                    arr[j - 1] = arr[j];
+//                    arr[j] = temp;
+//                }
+//            }
+            int j = i + 1;
+            while (j > 0) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+                j--;
+            }
         }
-
         return arr;
     }
 
-    public static void main(String[] args) {
+        public static void main (String[]args){
 
-        // (2번째 , idx=1, 값 =5)부터 시작해서, 자신의 앞 값과 비교.
-        int[] arr = {8, 5, 6, 2, 4};
+            // (2번째 , idx=1, 값 =5)부터 시작해서, 자신의 앞 값과 비교.
+            int[] arr = {8, 5, 6, 2, 4};
 
-        InsertionSort01 is = new InsertionSort01();
+            InsertionSort01 is = new InsertionSort01();
 
-        is.sort(arr);
+            is.sort(arr);
 
-        System.out.println(Arrays.toString(arr));
+            System.out.println(Arrays.toString(arr));
 
+        }
     }
-}
+
