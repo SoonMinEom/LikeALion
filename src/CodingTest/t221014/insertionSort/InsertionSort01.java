25d5991770f1f5255b565b8bfde8b3be146a8f74
soonmineom <skdlfma123@gmail.com>
2022-10-14 09:38:27 +0900

    InsertionSort 1단계

---
+++ src/CodingTest/t221014/insertionSort/InsertionSort01.java
@@ -0,0 +1,21 @@
+package CodingTest.t221014.insertionSort;
+
+public class InsertionSort01 {
+
+    public int[] sort (int[] arr) {
+        int i = 0;
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        // (2번째 , idx=1, 값 =5)부터 시작해서, 자신의 앞 값과 비교.
+        int[] arr = {8, 5, 6, 2, 4};
+
+        InsertionSort01 is = new InsertionSort01();
+
+        is.sort(arr);
+
+    }
+}
