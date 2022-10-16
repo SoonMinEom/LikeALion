c525442730132b462a9d69438565ab59da00765b
soonmineom <skdlfma123@gmail.com>
2022-10-16 10:09:09 +0900

    반복응용

---
+++ src/Homework/CodeUp/h221014/Main7.java
@@ -0,0 +1,21 @@
+package Homework.CodeUp.h221014;
+
+import java.util.Scanner;
+
+public class Main7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = 0;
+
+        while( b <= a) {
+
+            System.out.println(b);
+            b++;
+
+        }
+
+    }
+}
