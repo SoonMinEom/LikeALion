b8c8985bc49e6a71c3d72b8a4a008bfcc5c353d3
soonmineom <skdlfma123@gmail.com>
2022-10-16 09:58:00 +0900

    기초반복2

---
+++ src/Homework/CodeUp/h221014/Main5.java
@@ -0,0 +1,19 @@
+package Homework.CodeUp.h221014;
+
+import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        while( a > 0) {
+
+            a--;
+            System.out.println(a);
+
+        }
+
+    }
+}
