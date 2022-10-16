07f7983da97050cb05a211ed9737256090edebf1
soonmineom <skdlfma123@gmail.com>
2022-10-16 10:14:23 +0900

    반복응용

---
+++ src/Homework/CodeUp/h221014/Main9.java
@@ -0,0 +1,20 @@
+package Homework.CodeUp.h221014;
+
+import java.util.Scanner;
+
+public class Main9 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            char input = sc.next().charAt(0);
+            if (input == 'q') {
+                System.out.println(input);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+    }
+}
