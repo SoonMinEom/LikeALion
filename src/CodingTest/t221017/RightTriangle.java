a4f14e496b853f183a2bc218ef79f1498c88a6ce
soonmineom <skdlfma123@gmail.com>
2022-10-17 09:07:29 +0900

    반복응용

---
+++ src/CodingTest/t221017/RightTriangle.java
@@ -0,0 +1,12 @@
+package CodingTest.t221017;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+
+
+    }
+}
