4b6731544a191a710f7fc7d595929fe305f1a0c6
soonmineom <skdlfma123@gmail.com>
2022-10-17 09:36:42 +0900

    RightTriangle

--- src/CodingTest/t221017/RightTriangle.java
+++ src/CodingTest/t221017/RightTriangle.java
@@ -2,10 +2,20 @@ package CodingTest.t221017;
 
 public class RightTriangle {
 
+    String letter;
+
+    public RightTriangle(String letter) {
+        this.letter = letter;
+    }
+
+    public RightTriangle() {
+        this.letter = "*";
+    }
+
     public void RightTriangleMaker(int level) {
         for (int i = 0 ; i < level ; i++) {
             for (int j =0; j <i+1 ; j++) {
-                System.out.print("* ");
+                System.out.print(this.letter);
             }
             System.out.println();
         }
@@ -13,7 +23,7 @@ public class RightTriangle {
 
     public static void main(String[] args) {
 
-        RightTriangle rt = new RightTriangle();
+        RightTriangle rt = new RightTriangle("#");
         rt.RightTriangleMaker(10);
 
     }
